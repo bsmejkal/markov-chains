8b593a7be16ae928d4b4d88cdf7b9a69da73dbba
Your Name <you@example.com>
2020-02-04 12:54:49 -0800

    Completed functions for open & read and dictionary chains

---
+++ markov.py
@@ -0,0 +1,92 @@
+"""Generate Markov text from text files."""
+
+from random import choice
+
+
+def open_and_read_file(file_path):
+    """Take file path as string; return text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+
+    with open(file_path) as f:
+        text = f.read()
+
+    return text
+
+
+def make_chains(text_string):
+    """Take input text as string; return dictionary of Markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> chains = make_chains("hi there mary hi there juanita")
+
+    Each bigram (except the last) will be a key in chains:
+
+        >>> sorted(chains.keys())
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+    Each item in chains is a list of all possible following words:
+
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
+        
+        >>> chains[('there','juanita')]
+        [None]
+    """
+
+    chains = {}
+    words_list = []
+
+
+    text_string.strip()
+    words = text_string.split()
+
+    words_list.extend(words)
+
+    i = 0
+
+    for word in range(len(words_list) - 2):
+        key = (words_list[i], words_list[i + 1])
+        value = [words_list[i + 2]]
+
+        if key in chains:
+            chains[key].extend(value)
+        else:
+            chains[key] = value
+
+        i = i + 1
+
+    sorted(chains.keys())
+
+    return chains
+
+
+def make_text(chains):
+    """Return text from chains."""
+
+    words = []
+
+    # your code goes here
+
+    return " ".join(words)
+
+
+input_path = "green-eggs.txt"
+
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+
+# Get a Markov chain
+chains = make_chains(input_text)
+
+# Produce random text
+random_text = make_text(chains)
+
+print(random_text)
